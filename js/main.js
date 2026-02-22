d30fede00f0acba86c1bea2530a2f5f6abce4b99
Juancho <juancho@email.com>
2026-02-22 00:13:07 -0500

    Wallpapers final profesional

--- js/main.js
+++ js/main.js
@@ -1,67 +1,79 @@
-const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
+// ===== CANVAS PARTICULAS =====
+let canvas = document.getElementById("particles");
+
+if (!canvas) {
+  canvas = document.createElement("canvas");
+  canvas.id = "particles";
+  document.body.appendChild(canvas);
+}
+
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function resizeCanvas(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
 
 let particles = [];
 
 class Particle{
   constructor(){
-    this.x = Math.random()*canvas.width;
-    this.y = Math.random()*canvas.height;
-    this.size = Math.random()*2+1;
-    this.speedX = Math.random()*0.5-0.25;
-    this.speedY = Math.random()*0.5-0.25;
+    this.reset();
+  }
+
+  reset(){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = Math.random() * 0.5 - 0.25;
+    this.speedY = Math.random() * 0.5 - 0.25;
   }
 
   update(){
-    this.x+=this.speedX;
-    this.y+=this.speedY;
+    this.x += this.speedX;
+    this.y += this.speedY;
 
-    if(this.x<0 || this.x>canvas.width) this.speedX*=-1;
-    if(this.y<0 || this.y>canvas.height) this.speedY*=-1;
+    // Rebote suave
+    if(this.x <= 0 || this.x >= canvas.width){
+      this.speedX *= -1;
+    }
+    if(this.y <= 0 || this.y >= canvas.height){
+      this.speedY *= -1;
+    }
   }
 
   draw(){
-    ctx.fillStyle="#00ffcc";
+    ctx.fillStyle = "#00ffcc";
     ctx.beginPath();
-    ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-function init(){
-  particles=[];
-  for(let i=0;i<80;i++){
+function initParticles(){
+  particles = [];
+  for(let i = 0; i < 80; i++){
     particles.push(new Particle());
   }
 }
 
 function animate(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  particles.forEach(p=>{
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach(p => {
     p.update();
     p.draw();
   });
+
   requestAnimationFrame(animate);
 }
 
-init();
+initParticles();
 animate();
 
-window.addEventListener("resize",()=>{
-  canvas.width=window.innerWidth;
-  canvas.height=window.innerHeight;
-  init();
+window.addEventListener("resize", () => {
+  resizeCanvas();
+  initParticles();
 });
-
-function openModal(src){
-  document.getElementById("modal").style.display="block";
-  document.getElementById("modal-img").src=src;
-}
-
-function closeModal(){
-  document.getElementById("modal").style.display="none";
-}
