40913bc1b76b9b710e77bd8100abd512c0ca19d0
Juancho <juancho@email.com>
2026-02-21 22:44:45 -0500

    Actualización wallpapers y mejoras visuales

--- js/main.js
+++ js/main.js
@@ -1,1 +1,61 @@
-console.log("Juan Gamer Hub activo 🔥");
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+  constructor(){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 3 + 1;
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = Math.random() * 1 - 0.5;
+  }
+
+  update(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if(this.x > canvas.width || this.x < 0){
+      this.speedX *= -1;
+    }
+    if(this.y > canvas.height || this.y < 0){
+      this.speedY *= -1;
+    }
+  }
+
+  draw(){
+    ctx.fillStyle = "#00ffcc";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init(){
+  particlesArray = [];
+  for(let i = 0; i < 100; i++){
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  for(let i = 0; i < particlesArray.length; i++){
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+window.addEventListener("resize", function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
