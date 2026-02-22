e73a57a6739612b6bd76219e3faf7763c46c130c
Juancho <juancho@email.com>
2026-02-21 23:21:41 -0500

    Estructura completa profesional multi pagina

--- js/main.js
+++ js/main.js
@@ -1,69 +1,60 @@
-// PARTÍCULAS
-const canvas = document.getElementById("particles");
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particlesArray = [];
+canvas.style.position="fixed";
+canvas.style.top="0";
+canvas.style.left="0";
+canvas.style.zIndex="-1";
 
-class Particle {
+let particlesArray=[];
+
+class Particle{
   constructor(){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 3 + 1;
-    this.speedX = Math.random() * 1 - 0.5;
-    this.speedY = Math.random() * 1 - 0.5;
+    this.x=Math.random()*canvas.width;
+    this.y=Math.random()*canvas.height;
+    this.size=Math.random()*3+1;
+    this.speedX=Math.random()*1-0.5;
+    this.speedY=Math.random()*1-0.5;
   }
-
   update(){
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    if(this.x > canvas.width || this.x < 0){
-      this.speedX *= -1;
-    }
-    if(this.y > canvas.height || this.y < 0){
-      this.speedY *= -1;
-    }
+    this.x+=this.speedX;
+    this.y+=this.speedY;
+    if(this.x>canvas.width||this.x<0) this.speedX*=-1;
+    if(this.y>canvas.height||this.y<0) this.speedY*=-1;
   }
-
   draw(){
-    ctx.fillStyle = "#00ffcc";
+    ctx.fillStyle="#00ffcc";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
     ctx.fill();
   }
 }
 
 function init(){
-  particlesArray = [];
-  for(let i = 0; i < 100; i++){
+  particlesArray=[];
+  for(let i=0;i<100;i++){
     particlesArray.push(new Particle());
   }
 }
 
 function animate(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  for(let i = 0; i < particlesArray.length; i++){
-    particlesArray[i].update();
-    particlesArray[i].draw();
-  }
+  particlesArray.forEach(p=>{
+    p.update();
+    p.draw();
+  });
   requestAnimationFrame(animate);
 }
 
 init();
 animate();
 
-window.addEventListener("resize", function(){
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+window.addEventListener("resize",()=>{
+  canvas.width=window.innerWidth;
+  canvas.height=window.innerHeight;
   init();
-});
-
-// BOTÓN WALLPAPERS
-document.getElementById("go-wallpapers").addEventListener("click", function(){
-  document.getElementById("wallpapers").scrollIntoView({
-    behavior: "smooth"
-  });
 });
