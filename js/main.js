a869f03be31f2536e876f8029541c4534fb3478a
Juancho <juancho@email.com>
2026-02-21 23:33:32 -0500

    Sitio completo profesional multi pagina final

--- js/main.js
+++ js/main.js
@@ -5,27 +5,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-canvas.style.position="fixed";
-canvas.style.top="0";
-canvas.style.left="0";
-canvas.style.zIndex="-1";
-
-let particlesArray=[];
+let particles = [];
 
 class Particle{
   constructor(){
-    this.x=Math.random()*canvas.width;
-    this.y=Math.random()*canvas.height;
-    this.size=Math.random()*3+1;
-    this.speedX=Math.random()*1-0.5;
-    this.speedY=Math.random()*1-0.5;
+    this.x = Math.random()*canvas.width;
+    this.y = Math.random()*canvas.height;
+    this.size = Math.random()*2+1;
+    this.speedX = Math.random()*0.5-0.25;
+    this.speedY = Math.random()*0.5-0.25;
   }
+
   update(){
     this.x+=this.speedX;
     this.y+=this.speedY;
-    if(this.x>canvas.width||this.x<0) this.speedX*=-1;
-    if(this.y>canvas.height||this.y<0) this.speedY*=-1;
+
+    if(this.x<0 || this.x>canvas.width) this.speedX*=-1;
+    if(this.y<0 || this.y>canvas.height) this.speedY*=-1;
   }
+
   draw(){
     ctx.fillStyle="#00ffcc";
     ctx.beginPath();
@@ -35,15 +33,15 @@ class Particle{
 }
 
 function init(){
-  particlesArray=[];
-  for(let i=0;i<100;i++){
-    particlesArray.push(new Particle());
+  particles=[];
+  for(let i=0;i<80;i++){
+    particles.push(new Particle());
   }
 }
 
 function animate(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  particlesArray.forEach(p=>{
+  particles.forEach(p=>{
     p.update();
     p.draw();
   });
